a68febd79dd01a8611a5ce1c423bf9d7112b567a
Allken Vaneeda <vaneeda.shalini.devi.allken@hi.no>
2019-03-05 15:55:49 +0100

    Add docstring

--- generate.py
+++ generate.py
@@ -1,12 +1,10 @@
 import sys
-
 # check if python3 is used
 if sys.version_info < (3, 0):
     print("This programs need python3x to run. Aborting.")
     sys.exit(1)
 
 import os
-import glob
 import random
 from PIL import Image
 from math import floor
@@ -29,11 +27,28 @@ def initialize(backgrounds_dir, classes_dir):
 
 # Simulate an image
 def mkimage(filename, objs, names, bgs, maxobjs, output_dir="images_out",single=False):
+    """
+    Generates a synthetic image consisting of a random number of objects pasted on a random background
+    and a text file with coordinates of each object, along with class name.
+
+    Parameters:
+    :param filename: Prefix of image and text file generated
+    :param objs: List of lists of image crops
+    :param names: Class names
+    :param bgs: Directory of background images
+    :param maxobjs: Maximum number of objects generated in a given image
+    :param output_dir: Output directory, default="images_out"
+    :param single: Boolean indicating whether image contains a single species (True) or not
+
+    :Returns: png image and txt file
+    """
+
     log = []
     im = bgs[random.randint(0,len(bgs)-1)].copy()
     # print('bg size='+str(im.size))
     cls0 = random.randint(0,len(objs)-1)
-    for c in range(0,random.randint(1,maxobjs)):
+    for c in range(0, random.randint(1, maxobjs)):
+
         if single: cls=cls0
         else: cls = random.randint(0,len(objs)-1)
         obj = random.choice(objs[cls])
@@ -50,4 +65,4 @@ def mkimage(filename, objs, names, bgs, maxobjs, output_dir="images_out",single=
 # Testing
 def test():
     objects, names, backgrounds = initialize(backgrounds_dir="backgrounds", classes_dir="crops")
-    mkimage('test', objects, names, backgrounds, output_dir="images_out", maxobjs=6)
+    mkimage('train', objects, names, backgrounds, output_dir="images_out", maxobjs=6)
