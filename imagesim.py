38574487cbfd18af8bd6457a4de4d08600f1647b
Allken Vaneeda <vaneeda.shalini.devi.allken@hi.no>
2020-02-07 17:25:35 +0100

    Added default arguments for backgrounds, crops and max num of elements per image

--- imagesim.py
+++ imagesim.py
@@ -11,38 +11,38 @@ if sys.version_info < (3, 0):
 
 p = argparse.ArgumentParser(description='Generate images from background and elements.')
 p.add_argument('-d', '--path_to_directory'
-               , help='path_to_directory')
-p.add_argument('-b', '--backgrounds'
-               , help='directory containing background images')
-p.add_argument('-c', '--classes'
+               , help='path_to_directory containing backgrounds and crops folder')
+p.add_argument('-b', '--backgrounds', default = 'backgrounds'
+               , help='name of directory containing background images')
+p.add_argument('-c', '--classes', default = 'crops'
                , help='directory containing element images, one subdirectory per class')
 p.add_argument('-s', '--single', action='store_true'
                , help='generate images containing one class elements only')
 p.add_argument('-n'
                , help='number of images to generate')
-p.add_argument('-e'
+p.add_argument('-e', default = 6
                , help='max number of elements per image')
 p.add_argument('-o'
                , help='directory to store generated images')
 
 args = p.parse_args()
-print(args)
+
 backgrounds_dir = os.path.join(args.path_to_directory, args.backgrounds)
 classes_dir = os.path.join(args.path_to_directory, args.classes)
 objects, names, backgrounds = initialize(backgrounds_dir, classes_dir)
 n = int(args.n)
 
-if args.e == None: e=6
-else: e=int(args.e)
-
 output_dir = os.path.join(args.path_to_directory, args.o)
+
 for i in range(1,int(n)+1):
-    mkimage(args.o+'_%d' % int(i), objects, names, backgrounds, output_dir=output_dir, maxobjs=e, single=args.single)
+    mkimage(args.o+'_%d' % int(i), objects, names, backgrounds,
+            output_dir=output_dir, maxobjs=args.e, single=args.single)
 
 filename = args.o + "_annotations.csv"
 
 txt_files = os.path.join(output_dir, '*.txt')
 
+# Convert txt files to one csv annotation file
 with open(os.path.join(args.path_to_directory, filename), 'w') as output_file:
     for txt_file in glob.glob(txt_files):
         with open(txt_file, "r") as input_file:
